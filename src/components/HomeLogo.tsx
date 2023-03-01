2b3a435708c012602df31ceb4bfc9617c8622c63
mateusvg <mateusvitorino@outlook.com.br>
2023-03-01 15:56:59 -0300

    todo in home page

--- src/components/HomeLogo.tsx
+++ src/components/HomeLogo.tsx
@@ -5,7 +5,9 @@ export default function () {
     return (
             <Stack alignItems={'center'}>
                     Sistema de agendamento e atestado médico
-                    <img src={LogoX} alt="Logo" width={"40%"} />
+                    <img src={LogoX} alt="Logo" width={"20%"} />
+                    Clientes agendados do dia:...
+                    Colocar um get date:
             </Stack>
     )
 }
