35fbb3ec50988cb3aa07ba28ff6652588872f5dd
mateusvg <mateusvitorino@outlook.com.br>
2023-03-01 16:21:14 -0300

    search feature

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -7,12 +7,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
+import { Box, Button, Grid, Input, InputAdornment, TextField } from '@mui/material';
 import { getAllRegistersAdmin } from '../services/getAllRegistersTable';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 import CircleIcon from '../components/CircleIcon'
+import SearchIcon from '@mui/icons-material/Search';
+
 
 type resultProps = {
   idForm: string
@@ -28,6 +30,16 @@ type resultProps = {
 export default function DenseTable() {
 
 
+  const handleDownload = (anexo: any) => {
+    anexo = anexo.replace('data:image/png;base64,', '')
+    const payload = { anexo: anexo }
+    // console.log(payload.anexo)
+    var a = document.createElement("a"); //Create <a>
+    a.href = "data:image/png;base64," + payload.anexo; //Image Base64 Goes here
+    a.download = "Image.png"; //File name Here
+    a.click(); //Downloaded file
+  };
+
   //Get all registers
   const [result, setResult] = useState<resultProps[]>([]);
   const getAllRegisters = async () => {
@@ -38,6 +50,7 @@ export default function DenseTable() {
     getAllRegisters()
   }, [])
 
+  //Search
   const [searchInput, setSearchInput] = useState("")
   const handleChange = (e: any) => {
     e.preventDefault()
@@ -52,36 +65,50 @@ export default function DenseTable() {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell align="center">CPF</TableCell>
-            <TableCell align="center">Anexo</TableCell>
-            <TableCell align="center">Médico</TableCell>
-            <TableCell align="center">Aptidão</TableCell>
-            <TableCell align="center">Status</TableCell>
-            <TableCell align="center"> </TableCell>
-            <TableCell align="center">Editar</TableCell>
-            <TableCell align="center"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            result.filter(post => {
-              if (searchInput === '') {
-                return post;
-              } else if (post.cpf.toLowerCase().includes(searchInput.toLowerCase())) {
-                return post;
-              }
-            }).map((post, index) => (
+    <Box>
+      <Grid container justifyContent="center" marginBottom={5}>
+        <Input
+          id="input-with-icon-adornment"
+          placeholder="Procurar CPF"
+          onChange={handleChange}
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+        />
+      </Grid>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell align="center">CPF</TableCell>
+              <TableCell align="center">Anexo</TableCell>
+              <TableCell align="center">Médico</TableCell>
+              <TableCell align="center">Aptidão</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center"> </TableCell>
+              <TableCell align="center">Editar</TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              result.filter(post => {
+                if (searchInput === '') {
+                  return post;
+                } else if (post.cpf.toLowerCase().includes(searchInput.toLowerCase())) {
+                  return post;
+                }
+              }).map((post, index) => (
 
 
                 <TableRow>
                   <TableCell>{post.nomePaciente}</TableCell>
                   <TableCell>{post.cpf}</TableCell>
-                  <TableCell><Button ><DownloadForOfflineIcon /></Button></TableCell>
+                  <TableCell><Button onClick={() => handleDownload(post.anexo)}><DownloadForOfflineIcon /></Button></TableCell>
                   <TableCell>{post.nomeMedico}</TableCell>
                   <TableCell>{post.aptidao}</TableCell>
                   <TableCell>{post.Status}</TableCell>
@@ -91,10 +118,11 @@ export default function DenseTable() {
                 </TableRow>
 
 
-            ))
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
