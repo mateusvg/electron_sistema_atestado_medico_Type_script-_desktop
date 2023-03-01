7ae9626caf19860cf9bb57911e0157efca90eba4
mateusvg <mateusvitorino@outlook.com.br>
2023-03-01 16:40:30 -0300

    edit modal handle and layout

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -16,6 +16,9 @@ import CircleIcon from '../components/CircleIcon'
 import SearchIcon from '@mui/icons-material/Search';
 import ClearIcon from '@mui/icons-material/Clear';
 
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 
 type resultProps = {
   idForm: string
@@ -64,11 +67,16 @@ export default function DenseTable() {
       return data.cpf.includes(searchInput)
     })
   }
-
+  //clearSearh
   const handleClearClick = () => {
     setSearchInput("");
     console.log(searchInput)
   };
+
+  //MODAL
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <Box>
@@ -128,7 +136,7 @@ export default function DenseTable() {
                   <TableCell>{post.aptidao}</TableCell>
                   <TableCell>{post.Status}</TableCell>
                   <TableCell><CircleIcon /></TableCell>
-                  <TableCell><Button><EditIcon /></Button></TableCell>
+                  <TableCell><Button onClick={handleOpen}><EditIcon /></Button></TableCell>
                   <TableCell><Button><DeleteIcon /> </Button></TableCell>
                 </TableRow>
 
@@ -138,6 +146,36 @@ export default function DenseTable() {
           </TableBody>
         </Table>
       </TableContainer>
+
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
     </Box>
   );
 }
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
